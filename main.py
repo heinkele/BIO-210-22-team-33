32ef8d4255fec6e81f92c0138eee846a27bb2859
Salomé Peyrache <salome.peyrache@epfl.ch>
2022-10-24 15:07:49 +0200

    partie 3.2 faite avec mluefkens, heinkele, speyrache

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from winreg import HKEY_LOCAL_MACHINE
+import numpy as np
+N=50
+M=3
+
+my_list =[-1,1]
+a=[np.random.choice(my_list,N) for j in range(M)]
+W=np.zeros((50,50))
+for i in range (N):
+    for j in range(N):
+        if i==j : 
+            W[i][j]=0
+        else :
+            for k in range (M):
+                W[i][j]+=(1/M)*a[k][i]*a[k][j]
+                
 
 
-coucou bienvenue sur la team 33
