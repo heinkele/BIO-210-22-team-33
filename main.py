e47363827d7ad97d9ff0931a58e37afb7140e6a8
Mischa Luefkens <mischaluefkens@tsf-428-wpa-6-134.epfl.ch>
2022-10-31 15:07:17 +0100

    v2 faites avec speyrache heinkele et mluefkens

--- main.py
+++ main.py
@@ -1,51 +1,13 @@
 import numpy as np
-N=50
-M=3
+import functions as f
 
-my_list =[-1,1]
-a=[np.random.choice(my_list,N) for j in range(M)]
-W=np.zeros((50,50))
-for i in range (N):
-    for j in range(N):
-        if i==j : 
-            W[i][j]=0
-        else :
-            for k in range (M):
-                W[i][j]+=(1/M)*a[k][i]*a[k][j]
+memorized_patterns = f.generate_patterns(80,1000)
 
-a_0l = np.random.randint(3)
-a_1l = np.random.randint(3)
-a_2l = np.random.randint(3)
-a_0c = np.random.randint(50)
-a_1c = np.random.randint(50)
-a_2c = np.random.randint(50)
+# our chosen pattern is the 2nd line
+perturbed_pattern = f.perturb_pattern(memorized_patterns[2],200)
 
-while (a_0l == a_1l) and (a_0c == a_1c) :
-    while (a_0l == a_2l) and (a_0c == a_2c) :
-        while (a_1l == a_2l) and (a_1c == a_2c) :
+W = f.hebbian_weights(memorized_patterns)
 
-            a_1l = np.random.randint(3)
-            a_2l = np.random.randint(3)
-            a_1c = np.random.randint(50)
-            a_2c = np.random.randint(50)  ##generation de 3 indexations aléatoires différentes pour créer p_0
-
-p_0 = a
-
-p_0[a_0l][a_0c] *= -1
-p_0[a_1l][a_1c] *= -1
-p_0[a_2l][a_2c] *= -1       ##Changement de trois valeurs aléatoires dans W
-
-
-T = 0    ##Nb max itération bouce while 
-while (p_0 != a) and (T < 20) :   
-    for i in range (3) : 
-        for j in range (50) : 
-            if p_0[i][j] * W[i][j] >= 0 :
-                p_0[i][j] = 1
-            else :
-                p_0[i][j] = -1
-
-    T += 1
-
-print(W, "\n\n\n", p_0)
-
+dynamic_test = f.dynamics(perturbed_pattern,W,20)
+if (dynamic_test==memorized_patterns[2]):
+    print ("Vous etes trop forts")
