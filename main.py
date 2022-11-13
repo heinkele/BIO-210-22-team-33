b46ab1f4bd1bfd1afc2fb508822431d0c4ba4bff
Hugo <hugo.heinkele@epfl.ch>
2022-11-13 17:12:32 +0100

    Correction méthode perturbed_pattern, update et dynamics,et stokey rule avec heinkele et speyrache

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ memorized_patterns = f.generate_patterns(3,10)
 print("memorized pattern :", memorized_patterns[2])
 
 # our chosen pattern is the 2nd line
-perturbed_pattern = f.perturb_pattern(memorized_patterns[2],4)
+perturbed_pattern = f.perturb_pattern2(memorized_patterns[2],4)
 
 print ("perturb pattern :", perturbed_pattern)
 
+
+
 W = f.hebbian_weights(memorized_patterns)
+
 
 dynamic_test = f.dynamics(perturbed_pattern,W,20)
 if ((dynamic_test==memorized_patterns[2]).all()):
@@ -21,18 +24,22 @@ if ((async_dynamic_test==memorized_patterns[2]).all()):
     print ("Vous etes trop forts")
 """
 
+#W = f.storkey_weights(memorized_patterns)
 
-f.generate_checkerboard()
 
-memorized_patterns = f.generate_patterns(100,2500)
-memorized_patterns[2] = f.flatten_checkerboard(f.generate_initial_checkerboard())
-perturbed_pattern = f.perturb_pattern(memorized_patterns[2],1000)
-W = f.hebbian_weights(memorized_patterns)
-dynamic_test = f.dynamics(perturbed_pattern,W,20)
 
-if ((dynamic_test==memorized_patterns[2]).all()):
-    print ("Vous etes trop forts")
 
-async_dynamic_test = f.dynamics_async(perturbed_pattern,W,20000,3000)
-if ((async_dynamic_test==memorized_patterns[2]).all()):
-    print ("Vous etes trop forts")
+#f.generate_checkerboard()
+
+#memorized_patterns = f.generate_patterns(100,2500)
+#memorized_patterns[2] = f.flatten_checkerboard(f.generate_initial_checkerboard())
+#perturbed_pattern = f.perturb_pattern(memorized_patterns[2],1000)
+#W = f.hebbian_weights(memorized_patterns)
+#dynamic_test = f.dynamics(perturbed_pattern,W,20)
+
+#if ((dynamic_test==memorized_patterns[2]).all()):
+ #   print ("Vous etes trop forts")
+
+#async_dynamic_test = f.dynamics_async(perturbed_pattern,W,20000,3000)
+#if ((async_dynamic_test==memorized_patterns[2]).all()):
+ #   print ("Vous etes trop forts")
