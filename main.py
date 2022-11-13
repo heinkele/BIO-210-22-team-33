67f26e324964a24262dd5b5b028fd75e21781820
Salomé Peyrache <salome.peyrache@epfl.ch>
2022-11-13 17:14:05 +0100

    correction main avec heinkele et speyrache

--- main.py
+++ main.py
@@ -24,22 +24,23 @@ if ((async_dynamic_test==memorized_patterns[2]).all()):
     print ("Vous etes trop forts")
 """
 
-#W = f.storkey_weights(memorized_patterns)
+W = f.storkey_weights(memorized_patterns)
 
 
 
 
-#f.generate_checkerboard()
+f.generate_checkerboard()
 
-#memorized_patterns = f.generate_patterns(100,2500)
-#memorized_patterns[2] = f.flatten_checkerboard(f.generate_initial_checkerboard())
-#perturbed_pattern = f.perturb_pattern(memorized_patterns[2],1000)
-#W = f.hebbian_weights(memorized_patterns)
-#dynamic_test = f.dynamics(perturbed_pattern,W,20)
+memorized_patterns = f.generate_patterns(100,2500)
+memorized_patterns[2] = f.flatten_checkerboard(f.generate_initial_checkerboard())
+perturbed_pattern = f.perturb_pattern(memorized_patterns[2],1000)
+W = f.hebbian_weights(memorized_patterns)
+dynamic_test = f.dynamics(perturbed_pattern,W,20)
 
-#if ((dynamic_test==memorized_patterns[2]).all()):
- #   print ("Vous etes trop forts")
+if ((dynamic_test==memorized_patterns[2]).all()):
+    print ("Vous etes trop forts")
 
-#async_dynamic_test = f.dynamics_async(perturbed_pattern,W,20000,3000)
-#if ((async_dynamic_test==memorized_patterns[2]).all()):
- #   print ("Vous etes trop forts")
+async_dynamic_test = f.dynamics_async(perturbed_pattern,W,20000,3000)
+if ((async_dynamic_test==memorized_patterns[2]).all()):
+    print ("Vous etes trop forts")
+    
