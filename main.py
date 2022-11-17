d44185f2833f981c39e201db655f352d3a1037e7
Hugo <hugo.heinkele@epfl.ch>
2022-11-17 18:56:49 +0100

    Codage tests hebbian_weight et dynamics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import functions as f
-""" 
+
 memorized_patterns = f.generate_patterns(3,4)
 print("memorized pattern :", memorized_patterns[2]) # our chosen pattern is the 2nd line
 
@@ -13,6 +13,8 @@ print ("W :", W)
 dynamic_test = f.dynamics(perturbed_pattern,W,20)
 if ((dynamic_test==memorized_patterns[2]).all()):
     print ("Vous etes trop forts")
+
+"""
 
 async_dynamic_test = f.dynamics_async(perturbed_pattern,W,20000,3000)
 if ((async_dynamic_test==memorized_patterns[2]).all()):
--- test_functions.py
+++ test_functions.py
@@ -16,19 +16,45 @@ def test_perturb_pattern():
     assert t.count(-1) == 20
     assert all(m in t for m in [1, -1]) #value tests
     # exceptions treated in doctests
-"""
+""""
 --------------------------- HUGO -------------------------------
+
+"""
+
+def test_hebbian_weights():
+    # generic values
+    assert np.allclose(f.hebbian_weights([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]), ([[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]]))
+    memorized_patterns = f.generate_patterns(3,4)
+    hebbian_weight = f.hebbian_weights(memorized_patterns)
+    assert np.shape(hebbian_weight)[0] == 4  
+    assert np.shape(hebbian_weight)[0] == np.shape(hebbian_weight)[1]
+    for i in range (np.shape(hebbian_weight)[0]):
+        for j in range (np.shape(hebbian_weight)[1]):
+            assert (hebbian_weight[i][j] <= 1) and (hebbian_weight[i][j] >= -1)
+            assert hebbian_weight[i][j] == hebbian_weight[j][i]
+            if i==j :
+                assert hebbian_weight[i][j] == 0
+
 def test_update():
-    assert np.allclose(f.update(np.array([[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]]), ([-1, 1, -1, 1])), ([1, 1, -1, 1]))
+    assert np.allclose(f.update(np.array([[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]]), ([-1, 1, 1, 1])), ([-1, -1, -1, 1]))
 
 def test_dynamics():
     assert np.allclose(f.dynamics(([-1, 1, -1, 1]), [[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]], 20), [[1, 1, -1, 1], [1, 1, -1, 1]])
+    memorized_pattern = np.array([1, 1, -1, 1])
+    perturbed_pattern = np.array([-1, 1, 1, 1])  #3 perturbations
+    hebbian_weight = np.array([[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]])
+    assert np.allclose(f.dynamics(perturbed_pattern, hebbian_weight, 20)[-1], memorized_pattern)
+
+
+"""
 
 def test_update_async():
 
 def test_dynamics_async():
     hahj
 
+"""
+"""
 --------------------------- SALOME -------------------------------
 def test_storkey_weights():
     assert np.allclose(f.storkey_weights([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]), ([[1.125, 0.25, -0.25, -0.5], [0.25, 0.625, -1, 0.25], [-0.25, -1, 0.625, -0.25], [-0.5, 0.25, -0.25, 1.125]]))
@@ -44,15 +70,14 @@ def test_vector_to_matrix():
 
 -------------------------- MISCHA -------------------------------
 """
+"""
 def test_generate_initial_checkerboard():
     checkerboard = f.generate_initial_checkerboard()
     f.matrix_element_tests(checkerboard)
     #no easy way to compare a checkerboard with a premaid one 
 
- 
-def test_hebbian_weights():
-    # generic values
-    assert np.allclose(f.hebbian_weights([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]), ([[0, 1/3, -1/3, -1/3], [1/3, 0, -1, 1/3], [-1/3, -1, 0, -1/3], [-1/3, 1/3, -1/3, 0]]))
+
+"""
 """
 def test_matrix_list():
     assert np.allclose( f.matrix_list(list((np.ones(2500),-1*np.ones(2500)))) , list((np.ones(np.ones(50)), -1*np.ones(-1*np.ones(50)))) )
