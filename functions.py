8e366916ef13d010ca283f8a683ea853dd77ea5b
Hugo <hugo.heinkele@epfl.ch>
2022-11-20 16:11:27 +0100

    correction storkey rule

--- functions.py
+++ functions.py
@@ -229,19 +229,19 @@ def storkey_weights(patterns):
     num_patterns = np.shape(patterns)[0]
     pattern_size = np.shape(patterns)[1]
     W = np.zeros((pattern_size,pattern_size))
-    W_prev =  W #premier w que de 0
-    H=np.zeros((pattern_size, pattern_size))
+    W_prev =  W.copy() #premier w que de 0
 
     for u in range (num_patterns) :
-        H += W_prev@patterns[u]
-        np.reshape(H, (1, pattern_size*pattern_size))
+        H = W_prev@patterns[u]
+        H = np.reshape(H, (pattern_size, 1))
         case_i_eq_k = np.diag(W_prev)*patterns[u]
-        W_prev_diag_eq_0 = W_prev
+        case_i_eq_k = np.reshape(case_i_eq_k, (pattern_size, 1))
+        W_prev_diag_eq_0 = W_prev.copy()
         np.fill_diagonal(W_prev_diag_eq_0, 0)
         case_j_eq_k = W_prev_diag_eq_0*patterns[u]
-        H -= case_i_eq_k + case_j_eq_k
+        H = H - case_i_eq_k - case_j_eq_k
         np.reshape(H, (pattern_size, pattern_size))
-        W = W_prev + (1/num_patterns)*(np.outer(patterns[u], patterns[u]) - patterns[u] * H - np.transpose(patterns[u]*H))
+        W = W_prev + (1/pattern_size)*(np.outer(patterns[u], patterns[u]) - patterns[u] * H - np.transpose(patterns[u]*H))
         W_prev = W
     return W
   
